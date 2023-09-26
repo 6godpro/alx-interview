7469fb8089c2488d398ff1d29f8753928e633005
6godpro <erickson5115@gmail.com>
2023-09-27 00:04:33 +0100

    initial upload

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+    """Returns a list of integers representing
+    the Pascal's triangle of n."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(n - 1):
+        temp = [1]
+        for i in range(len(triangle[-1]) - 1):
+            temp.append(triangle[-1][i] + triangle[-1][i + 1])
+        temp.append(1)
+        triangle.append(temp)
+
+    return triangle
