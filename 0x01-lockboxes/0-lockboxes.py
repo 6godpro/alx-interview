abb77e39888e9fbc119bf0405832ff4c86a0b796
6godpro <erickson5115@gmail.com>
2023-10-04 23:04:52 +0100

    added new validation check to restrict keys to +ve values only

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,13 +14,27 @@ def validate_boxes_is_list_of_list(boxes):
     return all([type(box) is list for box in boxes])
 
 
+def keyIsValid(key, boxes, visited):
+    """A key is valid if it fits the following conditions.
+       -> It is an integer.
+       -> It is in this range, 0 >= key < len(boxes).
+       -> It is not in the visited list.
+    """
+    if type(key) is int and \
+            key >= 0 and \
+            key < len(boxes) and \
+            key not in visited:
+        return True
+    return False
+
+
 def canUnlockAll(boxes):
     """
        Verifies that all boxes contained in boxes
        can be unlocked.
        Args:
            boxes (list): A list of lists.
-       Return: True, if boxes contain lists only else False.
+       Return (boolean): True, if all boxes can be unlocked, else False
     """
     if len(boxes) == 0 or \
             not validate_boxes_is_list_of_list(boxes):
@@ -29,11 +43,11 @@ def canUnlockAll(boxes):
     visited = []
 
     def dfs(currentBox):
-        visited.append(currentBox)
-        for key in boxes[currentBox]:
-            if type(key) is int and \
-                    key not in visited and \
-                    key < len(boxes):
+        visited.append(currentBox)  # stash a used key
+        for key in boxes[currentBox]:  # get a new key using the current key
+            if keyIsValid(key, boxes, visited):
+                # explore using the new key only
+                # if the key hasn't already been used
                 dfs(key)
-    dfs(0)
+    dfs(0)  # unlock the first box
     return len(visited) == len(boxes)
